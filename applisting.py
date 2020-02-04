c8d7089d9bee9b38fbf487e23c2604f294361c7d
Tim <tim.e.mcguire@gmail.com>
2020-02-04 13:49:11 -0600

    Update applisting.py
    commenting about requirements

--- applisting.py
+++ applisting.py
@@ -1,7 +1,14 @@
 ## 
 ## Required:  provide your own crez
+## API Specs:  https://help.veracode.com/viewer/book-attachment/LMv_dtSHyb7iIxAQznC~9w/ow62TsB0BONRol~H1ZeE_A
+## "You install the Python authentication library to enable HMAC authentication. 
+## HMAC is a required security measure for using the Veracode APIs with your Python applications.
+## docs: https://help.veracode.com/reader/LMv_dtSHyb7iIxAQznC~9w/wYuaO2z0umMlM7O_lSyKIg
+
 
 boozerbane = ""
 assblirb = ""
 
-url = "https://api.veracode.com/
+url = "https://api.veracode.com/appsec/v1/applications"
+
+
